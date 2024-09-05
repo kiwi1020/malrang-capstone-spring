29a5893801f53e9e3911264021a927c6e8e88471
kiwi1020 <108050749+kiwi1020@users.noreply.github.com>
2024-09-05 17:29:00 +0900

    gitignore.

--- src/test/java/com/malrang/controller/ChatControllerTest.java
+++ src/test/java/com/malrang/controller/ChatControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
 import java.util.Collections;
 
 import static org.mockito.Mockito.when;
